aa7a903d8dfeaa6285e75216aaec0e2a63515a69
Kristian Høgsberg <krh@bitplanet.net>
2015-05-13 11:36:01 +0200

    i965: do not emit_bool_to_cond_code with ssbo load expressions
    We do the same with ubo load expressions.

--- src/mesa/drivers/dri/i965/brw_fs_visitor.cpp
+++ src/mesa/drivers/dri/i965/brw_fs_visitor.cpp
@@ -2774,7 +2774,8 @@ fs_visitor::emit_bool_to_cond_code(ir_rvalue *ir)
 {
    ir_expression *expr = ir->as_expression();
 
-   if (!expr || expr->operation == ir_binop_ubo_load) {
+   if (!expr || expr->operation == ir_binop_ubo_load ||
+       expr->operation == ir_binop_ssbo_load) {
       ir->accept(this);
 
       fs_inst *inst = emit(AND(reg_null_d, this->result, fs_reg(1)));
@@ -2906,7 +2907,8 @@ fs_visitor::emit_if_gen6(ir_if *ir)
 {
    ir_expression *expr = ir->condition->as_expression();
 
-   if (expr && expr->operation != ir_binop_ubo_load) {
+   if (expr && expr->operation != ir_binop_ubo_load &&
+       expr->operation != ir_binop_ssbo_load) {
       fs_reg op[3];
       fs_inst *inst;
       fs_reg temp;
