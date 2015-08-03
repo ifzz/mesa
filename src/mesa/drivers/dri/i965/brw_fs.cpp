af951f547aa1c8f4d0c2b707a949ec59c5e4ef0f
Connor Abbott <connor.w.abbott@intel.com>
2015-08-03 15:00:51 -0700

    i965/fs: add support for printing double immediates

--- src/mesa/drivers/dri/i965/brw_fs.cpp
+++ src/mesa/drivers/dri/i965/brw_fs.cpp
@@ -4708,6 +4708,9 @@ fs_visitor::dump_instruction(backend_instruction *be_inst, FILE *file)
          case BRW_REGISTER_TYPE_F:
             fprintf(file, "%ff", inst->src[i].fixed_hw_reg.dw1.f);
             break;
+         case BRW_REGISTER_TYPE_DF:
+            fprintf(file, "%fdf", inst->src[i].fixed_hw_reg.dw1.df);
+            break;
          case BRW_REGISTER_TYPE_W:
          case BRW_REGISTER_TYPE_D:
             fprintf(file, "%dd", inst->src[i].fixed_hw_reg.dw1.d);
