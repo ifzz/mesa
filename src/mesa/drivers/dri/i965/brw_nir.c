04c1703a33e6de89f26375efcd3ba10c3f0abf50
Connor Abbott <connor.w.abbott@intel.com>
2015-08-03 15:02:05 -0700

    i965: use double lowering pass

--- src/mesa/drivers/dri/i965/brw_nir.c
+++ src/mesa/drivers/dri/i965/brw_nir.c
@@ -192,6 +192,10 @@ nir_optimize(nir_shader *nir, bool is_scalar)
          nir_validate_shader(nir);
       }
 
+      nir_lower_doubles(nir, nir_lower_drcp |
+                             nir_lower_dsqrt |
+                             nir_lower_drsq);
+      nir_validate_shader(nir);
       progress |= nir_copy_prop(nir);
       nir_validate_shader(nir);
       progress |= nir_opt_dce(nir);
