ca7bcc01c5330a3cd11748f2767c6abc8037ff4d
Connor Abbott <connor.w.abbott@intel.com>
2015-08-07 08:38:19 -0700

    nir: add a pass for lowering pack/unpackDouble2x32

--- src/glsl/nir/nir.h
+++ src/glsl/nir/nir.h
@@ -2020,7 +2020,9 @@ void nir_lower_to_source_mods(nir_shader *shader);
 
 bool nir_lower_gs_intrinsics(nir_shader *shader);
 
-bool nir_normalize_cubemap_coords(nir_shader *shader);
+void nir_lower_double_pack(nir_shader *shader);
+
+void nir_normalize_cubemap_coords(nir_shader *shader);
 
 void nir_live_variables_impl(nir_function_impl *impl);
 bool nir_ssa_defs_interfere(nir_ssa_def *a, nir_ssa_def *b);
