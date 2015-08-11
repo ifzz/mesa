1901fef88b267b198f02f773a07b8c58ee9f4979
Connor Abbott <connor.w.abbott@intel.com>
2015-08-11 16:16:42 -0700

    i965/fs: don't propagate cmod when the exec sizes differ
    This can happen when the source of the compare was split by the SIMD
    lowering pass. Potentially, we could allow the case where the exec size
    of scan_inst is larger, and scan_inst has the right quarter selected,
    but doing that seems a little more risky.
    
    Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>

--- src/mesa/drivers/dri/i965/brw_fs_cmod_propagation.cpp
+++ src/mesa/drivers/dri/i965/brw_fs_cmod_propagation.cpp
@@ -93,6 +93,9 @@ opt_cmod_propagation_local(bblock_t *block)
                 scan_inst->dst.reg_offset != inst->src[0].reg_offset)
                break;
 
+            if (scan_inst->exec_size != inst->exec_size)
+               break;
+
             /* CMP's result is the same regardless of dest type. */
             if (inst->conditional_mod == BRW_CONDITIONAL_NZ &&
                 scan_inst->opcode == BRW_OPCODE_CMP &&
