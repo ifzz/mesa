29746ca13436f91e91f5787745a007784359b734
Connor Abbott <connor.w.abbott@intel.com>
2015-08-14 10:18:39 -0700

    nir: add nir_src_bit_size() helper
    v2: use a ternary (Jason)

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -557,6 +557,12 @@ nir_dest_for_reg(nir_register *reg)
    return dest;
 }
 
+static inline unsigned
+nir_src_bit_size(nir_src src)
+{
+   return src.is_ssa ? src.ssa->bit_size : src.reg.reg->bit_size;
+}
+
 void nir_src_copy(nir_src *dest, const nir_src *src, void *instr_or_if);
 void nir_dest_copy(nir_dest *dest, const nir_dest *src, nir_instr *instr);
 
