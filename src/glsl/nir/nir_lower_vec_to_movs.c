2b2af866f296482bdda578717c992bc8c5829a00
Eduardo Lima Mitev <elima@igalia.com>
2015-08-25 21:55:37 +0200

    nir: Coalesce registers during lower_vec_to_movs() pass in certain conditions (WIP)
    This patch extends the lower_vec_to_movs() pass propagating the destination
    of a VECX instruction to the parent instructions of its sources, if these
    instructions meet certain conditions.
    
    This effectively avoids emitting IMOV instructions when it is not necessary,
    delivering better code into backends.
    
    This patch is work in progress, and it is being used now as playground to
    experiment with how a register coalesce pass would work on a NIR shader
    in non-SSA form.
    
    shader-db results before this patch:
    
    total instructions in shared programs: 1853747 -> 1804942 (-2.63%)
    instructions in affected programs:     1687580 -> 1638775 (-2.89%)
    helped:                                6748
    HURT:                                  11101
    GAINED:                                0
    LOST:                                  0
    
    shader-db results after this patch:
    
    total instructions in shared programs: 1853747 -> 1754087 (-5.38%)
    instructions in affected programs:     1680819 -> 1581159 (-5.93%)
    helped:                                8272
    HURT:                                  8875
    GAINED:                                0
    LOST:                                  0
    
    No piglit regressions observed.

--- src/glsl/nir/nir_lower_vec_to_movs.c
+++ src/glsl/nir/nir_lower_vec_to_movs.c
@@ -83,6 +83,52 @@ insert_mov(nir_alu_instr *vec, unsigned start_channel,
    return mov->dest.write_mask;
 }
 
+/**
+ * Clone an ALU instruction and override the destination with the one given by
+ * new_dest. It copies sources from original ALU to the new one, adjusting
+ * their swizzles.
+ *
+ * Returns the new ALU instruction.
+ */
+static nir_alu_instr *
+clone_alu_instr_and_override_dest(nir_alu_instr *alu_instr,
+                                  nir_alu_dest *new_dest, unsigned index,
+                                  void *mem_ctx)
+{
+   nir_alu_instr *new_alu_instr = nir_alu_instr_create(mem_ctx, alu_instr->op);
+
+   /* Determine which dest channel was used in the parent ALU instruction */
+   unsigned channel;
+   for (unsigned i = 0; i < 4; i++) {
+      if (alu_instr->dest.write_mask & (1 << i)) {
+         channel = i;
+         break;
+      }
+   }
+   // assert(alu_instr->dest.write_mask == (1 << channel));
+
+   for (unsigned i = 0; i < nir_op_infos[alu_instr->op].num_inputs; i++) {
+      nir_alu_src_copy(&new_alu_instr->src[i], &alu_instr->src[i], mem_ctx);
+
+      switch (alu_instr->op) {
+      case nir_op_fdot2:
+      case nir_op_fdot3:
+      case nir_op_fdot4:
+         continue;
+      default:
+         break;
+      }
+
+      new_alu_instr->src[i].swizzle[index] =
+         alu_instr->src[i].swizzle[channel];
+   }
+
+   nir_alu_dest_copy(&new_alu_instr->dest, new_dest, mem_ctx);
+   new_alu_instr->dest.write_mask = 1 << index;
+
+   return new_alu_instr;
+}
+
 static bool
 lower_vec_to_movs_block(nir_block *block, void *mem_ctx)
 {
@@ -106,12 +152,79 @@ lower_vec_to_movs_block(nir_block *block, void *mem_ctx)
 
       unsigned finished_write_mask = 0;
 
+      for (unsigned i = 0; i < 4; i++) {
+         if (!(vec->dest.write_mask & (1 << i)))
+            continue;
+
+         /* We don't propagate constants by now
+          * @FIXME: we could also consider propagating destination of
+          * load_const instructions.
+          */
+         if (vec->src[i].src.is_ssa)
+            continue;
+
+         nir_register *reg = vec->src[i].src.reg.reg;
+
+         nir_foreach_def_safe(reg, src) {
+            nir_instr *parent_instr = src->reg.parent_instr;
+
+            /* We only coalesce registers written by ALU instructions, by now.
+             * @FIXME: consider other type of instructions, like intrinsics, etc.
+             */
+            if (parent_instr->type != nir_instr_type_alu)
+               continue;
+
+            nir_alu_instr *parent_alu_instr = nir_instr_as_alu(parent_instr);
+            nir_register *parent_dest_reg = parent_alu_instr->dest.dest.reg.reg;
+
+            /* We only override dest registers that are only used in this vecX
+             * instruction.
+             * @FIXME: In the future we might consider registers used more than
+             * once as sources of the same vecX instruction.
+             */
+            if (list_length(&parent_dest_reg->uses) != 1)
+               continue;
+
+            /* @FIXME: by now I want to have fine-grained control over what ALU
+             * ops are safe to propagate. The switch below can be used to
+             * enable/disable this pass for the different ALU opcodes.
+             * I have detected that propagating imov ops doesn't work, but still
+             * have not analyzed why.
+             */
+            switch (parent_alu_instr->op) {
+            case nir_op_imov:
+               continue;
+            default:
+               break;
+            }
+
+            nir_alu_instr *new_alu_instr =
+               clone_alu_instr_and_override_dest(parent_alu_instr, &vec->dest,
+                                                 i, mem_ctx);
+            finished_write_mask |= new_alu_instr->dest.write_mask;
+
+            /* Remove the old ALU instruction */
+            nir_instr_remove(&parent_alu_instr->instr);
+            ralloc_free(parent_alu_instr);
+
+            /* Remove the intermediate register, if not used anymore */
+            if (list_length(&parent_dest_reg->defs) == 0)
+               nir_reg_remove(parent_dest_reg);
+
+            /* Insert the new instruction with the overwritten destination */
+            nir_instr_insert_before(&vec->instr, &new_alu_instr->instr);
+         }
+      }
+
       /* First, emit a MOV for all the src channels that are in the
        * destination reg, in case other values we're populating in the dest
        * might overwrite them.
        */
       for (unsigned i = 0, src_idx = 0; i < 4; i++) {
          if (!(vec->dest.write_mask & (1 << i)))
+            continue;
+
+         if (!(finished_write_mask & (1 << i)))
             continue;
 
          if (src_matches_dest_reg(&vec->dest.dest, &vec->src[src_idx].src)) {
