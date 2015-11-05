92f566b6b761a30c2478310620c876ee46fd74a6
Iago Toral Quiroga <itoral@igalia.com>
2015-11-05 11:57:15 +0100

    nir: fix output type for u2f

--- src/glsl/nir/nir_opcodes.py
+++ src/glsl/nir/nir_opcodes.py
@@ -169,7 +169,7 @@ unop_convert("b2f", tbool, tfloat32, "src0 ? 1.0f : 0.0f")
 # Int-to-boolean conversion
 unop_convert("i2b", tint32, tbool, "src0 != 0")
 unop_convert("b2i", tbool, tint32, "src0 ? 1 : 0") # Boolean-to-int conversion
-unop_convert("u2f", tuint32, tfloat, "src0") #Unsigned-to-float conversion.
+unop_convert("u2f", tuint32, tfloat32, "src0") #Unsigned-to-float conversion.
 
 unop_convert("d2f", tfloat64, tfloat32, "src0") # Single to double precision
 unop_convert("f2d", tfloat32, tfloat64, "src0") # Double to single precision
